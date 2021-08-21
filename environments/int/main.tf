6e71a8b5467f7e4f9392a86f4f699755a9323654
Dimitrios-Nikolaos Sinogeorgos <dnsinogeorgos@protonmail.ch>
2021-08-21 19:22:38 +0300

    add aws addons

--- environments/int/main.tf
+++ environments/int/main.tf
@@ -122,6 +122,27 @@ module "eks_cluster" {
   context = module.this.context
 }
 
+resource "aws_eks_addon" "vpc_cni" {
+  cluster_name      = module.eks_cluster.eks_cluster_id
+  resolve_conflicts = "OVERWRITE"
+  addon_name        = "vpc-cni"
+  addon_version     = "v1.9.0-eksbuild.1"
+}
+
+resource "aws_eks_addon" "coredns" {
+  cluster_name      = module.eks_cluster.eks_cluster_id
+  resolve_conflicts = "OVERWRITE"
+  addon_name        = "coredns"
+  addon_version     = "v1.8.4-eksbuild.1"
+}
+
+resource "aws_eks_addon" "kube_proxy" {
+  cluster_name      = module.eks_cluster.eks_cluster_id
+  resolve_conflicts = "OVERWRITE"
+  addon_name        = "kube-proxy"
+  addon_version     = "v1.21.2-eksbuild.2"
+}
+
 // https://aws.amazon.com/blogs/compute/cost-optimization-and-resilience-eks-with-spot-instances/
 module "eks_node_group_amd64_gp" {
   source  = "cloudposse/eks-node-group/aws"
